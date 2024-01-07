8c76546a87b4a5added59fc589ca16e634d66bcc
Allan Soriano <alsoriano@letran-manaoag.edu.ph>
2024-01-07 10:16:33 +0800

    Updated Hello to Hi

--- Chatroom/index.php
+++ Chatroom/index.php
@@ -18,7 +18,7 @@
     <!-- <script src="scripts/chatbox.js" defer></script> -->
 </head>
 <body id="statediv">
-    <!-- JUST SOME CHANGES! -->
+    <!-- Hi! -->
     <?php 
     if (isset($_SESSION['useruid'])) {
         $username = $_SESSION['useruid'];
